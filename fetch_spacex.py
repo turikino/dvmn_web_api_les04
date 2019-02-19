a5b43369a3d58bfa9e98cc5f24e862e2112a3a85
turikino <turikino84@gmail.com>
2019-02-19 14:40:47 +0300

    Добавил применение argparse в fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,5 +1,13 @@
 import requests
 import os
+import argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Скачивает фото с последнего запуска SpaceX.')
+    parser.add_argument('-p', '--path', help='Путь к папке:')
+    parser.add_argument('-f', '--filename', help='Название для файла:')
+    return parser
 
 
 def fetch_spacex_last_lanch(path, filename):
@@ -18,3 +26,11 @@ def fetch_spacex_last_lanch(path, filename):
         full_name = '{}{}{}.jpeg'.format(path, filename, number + 1)
         with open(full_name, 'wb') as file:
             file.write(response.content)
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    path = args.path
+    filename = args.filename
+    fetch_spacex_last_lanch(path, filename)
